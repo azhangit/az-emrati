718e2c6ab4174356ee2b0926559fe3e1cf081c47
Azhan Git <azhanxbid2005@gmail.com>
2026-01-08 13:20:48 +0500

    controller.php update for new select images of modal

--- app/Http/Controllers/AizUploadController.php
+++ app/Http/Controllers/AizUploadController.php
@@ -138,6 +138,9 @@ class AizUploadController extends Controller
                 }
 
                 $path = $request->file('aiz_file')->store('uploads/all', 'local');
+                if ($path === false) {
+                     return '{}';
+                }
                 $size = $request->file('aiz_file')->getSize();
 
                 // Return MIME type ala mimetype extension
